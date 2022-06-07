92527e1cc77919ec8bae39ff4315285698f3d343
Ben Cosmin <94783151+Ben-C3012@users.noreply.github.com>
2022-06-07 15:55:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// Get UI Elements 
+const button = document.querySelector('.expand-button')
+let cardBodys = document.querySelectorAll('.card-body')
+let cardHeaders = document.querySelectorAll('.card-header')
+const icons = document.querySelectorAll('i')
+const cards = document.querySelectorAll('.card')
+
+
+
+
+console.log(cards);
+
+
+cards[0].addEventListener('click', (e) => {
+        cardBodys[0].classList.toggle('active')
+        icons[0].classList.toggle('active')
+  
+  })
+
+  
+cards[1].addEventListener('click', (e) => {
+  cardBodys[1].classList.toggle('active')
+  icons[1].classList.toggle('active')
+
+})
+
+cards[2].addEventListener('click', (e) => {
+  cardBodys[2].classList.toggle('active')
+  icons[2].classList.toggle('active')
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
